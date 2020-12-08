7887484d3dbd3d975aaabe21fe82e638ba2c9816
codebietjhs <codebiet@gmail.com>
2020-12-08 17:35:14 +0530

    model location update

--- static/predict.js
+++ static/predict.js
@@ -3,7 +3,9 @@ let model;
 (async function () {
     // model = await tf.loadLayersModel('http://127.0.0.1:3000/json_model/model.json');
     // model = await tf.loadLayersModel('localstorage://HDR_model_1.keras');
-    model = await tf.loadLayersModel('file:///app/static/json_model/model.json');
+    // model = await tf.loadLayersModel('file:///app/static/json_model/model.json');
+    console.log(String(window.location.href));
+    model = await tf.loadLayersModel(String(window.location.href) + 'static/json_model/model.json')
     console.log('Model Loaded');
 })();
 
