fc7b086f42b3a7aae5b193d15343da7df9d4724b
codebietjhs <codebiet@gmail.com>
2020-12-08 17:27:58 +0530

    allowing CORS request

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.use(function (req, res, next) {
     next();
 });
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.use(express.static('static'))
 
